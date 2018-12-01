13df424ea8acc06ab8d9654d1f9c7b276bd12e5f
Lukas Neicelis <lukas.neicelis@shopify.com>
2018-12-01 17:05:54 +0200

    Removing access control from DateTimeTransformer

--- src/Transformer/DateTimeTransformer.php
+++ src/Transformer/DateTimeTransformer.php
@@ -4,9 +4,8 @@ namespace Lneicelis\Transformer\Transformer;
 
 use Lneicelis\Transformer\Contract\CanTransform;
 use DateTime;
-use Lneicelis\Transformer\Contract\HasAccessControl;
 
-class DateTimeTransformer implements CanTransform, HasAccessControl
+class DateTimeTransformer implements CanTransform
 {
     public static function getSourceClass(): string
     {
@@ -20,23 +19,5 @@ class DateTimeTransformer implements CanTransform, HasAccessControl
     public function transform($source)
     {
         return $source->format(DateTime::ISO8601);
-    }
-
-    /**
-     * @return string[]
-     */
-    public function getDefaultAcl(): array
-    {
-        return [];
-    }
-
-    /**
-     * @return array[]
-     */
-    public function getPropertyAcl(string $property): array
-    {
-        return [
-            'id' => ['owner'],
-        ];
     }
 }
